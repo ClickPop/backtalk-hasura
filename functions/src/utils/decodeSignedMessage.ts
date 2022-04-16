3ac4ce0de5ab1c9b13cb0fa86f4234793512e3ef
Graham Vasquez <rescus1221@gmail.com>
2022-04-16 00:41:33 -0400

    Fixed broken signer message

--- functions/src/utils/decodeSignedMessage.ts
+++ functions/src/utils/decodeSignedMessage.ts
@@ -5,10 +5,11 @@ import {
   bufferToHex,
   publicToAddress,
 } from 'ethereumjs-util';
+import { SIGNER_MESSAGE } from 'src/config/env';
 
 export const decodeSignedMessage = (msg: string) => {
   try {
-    const data = 'Hello';
+    const data = SIGNER_MESSAGE;
     const sig = hashPersonalMessage(Buffer.from(data));
     const sigParams = fromRpcSig(msg);
     const publicKey = ecrecover(sig, sigParams.v, sigParams.r, sigParams.s);
