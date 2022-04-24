fbe2200929840ac3ead06782ac9d45142f746e4c
Graham Vasquez <rescus1221@gmail.com>
2022-04-23 23:02:36 -0400

    Switched survey_responses to give back a JSON object instead of a bunch of arrays to reduce lookups

---
+++ hasura/migrations/default/1650739526330_recreate_survey_responses_with_jsonb_aggregation/down.sql
@@ -0,0 +1,15 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE VIEW "public"."survey_responses" AS
+--     SELECT
+--         responses.wallet,
+--         jsonb_object_agg(responses.question_id, responses.response_content) AS question_responses,
+--         jsonb_object_agg(responses.question_id, responses.response_option_id) as multiple_choice_option_ids,
+--         surveys.id,
+--         max(responses.created_at) AS created_at,
+--         max(responses.token_count) AS token_count
+--     FROM ((surveys
+--         RIGHT JOIN questions ON ((surveys.id = questions.survey_id)))
+--         RIGHT JOIN responses ON ((questions.id = responses.question_id)))
+--     GROUP BY responses.wallet, surveys.id
+--     ORDER BY (max(responses.created_at));
